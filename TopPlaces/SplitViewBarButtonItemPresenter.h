5d530bcac9453482c71c30a122d662ece2c8f3f4
Shitian Long <longst.cn@gmail.com>
2012-08-03 16:48:33 +0200

    add Tabbar delegate

---
+++ TopPlaces/SplitViewBarButtonItemPresenter.h
@@ -0,0 +1,15 @@
+//
+//  SplitViewBarButtonItemPresenter.h
+//  TopPlaces
+//
+//  Created by Shitian Long on 8/3/12.
+//  Copyright (c) 2012 OptiCaller Software AB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol SplitViewBarButtonItemPresenter <NSObject>
+
+@property (nonatomic, strong) UIBarButtonItem *splitViewBarButtonItem;
+
+@end

ef5d576ddbf3408b1d18bcb1299fc6f22244cf50
Shitian Long <longst.cn@gmail.com>
2012-08-06 10:08:51 +0200

    add assignment 5 task 5

--- TopPlaces/MapViewController.h
+++ TopPlaces/MapViewController.h
@@ -7,9 +7,22 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+
+@class MapViewController;
+
+@protocol MapViewControllerDelegate <NSObject>
+
+- (UIImage *)mapViewController:(MapViewController *)sender imageForAnnotation:(id <MKAnnotation>)annotation;
+
+@end
 
 @interface MapViewController : UIViewController
 
 @property (nonatomic, strong) NSArray *annotations;
+@property (nonatomic, weak) id <MapViewControllerDelegate> delegate;
+
+
 
 @end
--- TopPlaces/MapViewController.m
+++ TopPlaces/MapViewController.m
@@ -7,24 +7,27 @@
 //
 
 #import "MapViewController.h"
-#import <MapKit/MapKit.h>
+#import "PhotoViewController.h"
+#import "FlickrPhotoAnnotation.h"
 
-@interface MapViewController ()
+@interface MapViewController () <MKMapViewDelegate>
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
-
 
 @end
 
 @implementation MapViewController
 @synthesize mapView = _mapView;
 @synthesize annotations = _annotations;
+@synthesize delegate = _delegate;
 
 
+#pragma mark setter and getter
 - (void)setMapView:(MKMapView *)mapView{
     _mapView = mapView;
     [self updateMapView];
 }
+
 
 - (void)setAnnotations:(NSArray *)annotations{
     _annotations = annotations;
@@ -32,17 +35,69 @@
 }
 
 
+
 // lecture 11 around 46:57, when I was seguing, the outlet is not set, in this case, self.mapView is not set
 // therefore, we have to make view and model sync like this
 - (void)updateMapView{
     if(self.mapView.annotations) [self.mapView removeAnnotations:self.mapView.annotations];
     if (self.annotations) [self.mapView addAnnotations:self.annotations];
+}
+
+
+#pragma mark MK MapViewDelegate
+- (MKAnnotationView *) mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
+    MKAnnotationView *aView = [mapView dequeueReusableAnnotationViewWithIdentifier:@"MapVc"];
+    if (!aView) {
+        aView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"MapVc"];
+        aView.canShowCallout = YES;
+        // 30 30 from lecture 11
+        aView.leftCalloutAccessoryView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
+    }
+    aView.annotation = annotation;
+    [(UIImageView *)aView.leftCalloutAccessoryView setImage:nil];
+    return aView;
+}
+
+
+- (PhotoViewController *)splitViewPhotoViewController{
+    id photoViewController = [self.splitViewController.viewControllers lastObject];
+    
+    if (![photoViewController isKindOfClass:[PhotoViewController class]]) {
+        photoViewController = nil;
+    }
+    return photoViewController;
+}
+
+
+// TODO add a button show detail image
+// Think about if it possible first show callout, next chick show detail
+- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view{
+    UIImage *image = [self.delegate mapViewController:self imageForAnnotation:view.annotation];
+    // implement show detal view
+    [(UIImageView *)view.leftCalloutAccessoryView setImage:image];
+    
+    FlickrPhotoAnnotation *fpa = (FlickrPhotoAnnotation *)view.annotation;
+    if ([self splitViewPhotoViewController]) {
+        [[self splitViewPhotoViewController] setPhoto:fpa.photo];
+    }
+    else{
+        [self performSegueWithIdentifier:@"show photo detail" sender:fpa.photo];
+    }
 
 }
 
-- (void)viewDidAppear:(BOOL)animated{
-    [super viewDidAppear:animated];
-    [self updateMapView];
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    if ([segue.identifier isEqualToString:@"show photo detail"]) {
+        [[segue destinationViewController] setPhoto:sender];
+    }
+}
+
+
+#pragma mark view life cycle
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    self.mapView.delegate = self;
 }
 
 
@@ -52,6 +107,7 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
+
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
